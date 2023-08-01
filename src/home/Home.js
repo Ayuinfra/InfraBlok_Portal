cde50fc43668055ee51a884cb60e4ae8257e292a
Ashmit Tyagi <atyagi@infrablok.com>
2023-08-01 10:59:28 +0530

    Code Updated

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,18 +1,21 @@
 import { Link, Outlet } from "react-router-dom";
 import React from "react";
-import hback from "../assets/hback.jpeg"
-
+import img1 from "../assets/1home.jpg"
+import img2 from "../assets/2home.jpg"
+import img3 from "../assets/3home.webp"
+import img4 from "../assets/4home-transformed.jpeg"
+import image from "../assets/font.png"
 const Home = () => {
 
     return (
 <>
 <>
-         <div style={{ backgroundImage:`url(${hback})`, width:"100%" ,height:"100vh"}}>
-                    <h1 style={{ color: "red" }}>InfraBlok Portal</h1>
-                    <p style={{ color: "white", fontSize: "20px" }}>
-                     hii
-                    </p>
-                    <Link to="/teamDetails" />
+         <div style={{flex:1}}>
+           <div>
+           <img src={image}  style={{width:"100%",height:"89vh"}}/>
+           </div>
+          
+                  
 <Outlet/>
                   
                 </div>
